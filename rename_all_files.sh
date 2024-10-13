afce2d406d53959f2e4797a9cf13a37020e758fb
Francisco Martinez <franciscomj345@gmail.com>
2024-10-12 18:30:04 -0600

    loop

--- rename_all_files.sh
+++ rename_all_files.sh
@@ -6,8 +6,15 @@ unwanted_emoji_pattern='[\x{1F600}-\x{1F64F}\x{1F300}-\x{1F5FF}\x{1F680}-\x{1F6F
 
 name_files_in_folder(){
     local folder=$1
-  echo "hello"
-  echo ${folder}
+
+    find "$folder" -type f ! -name ".*" | while IFS= read -r file; do
+        base_name=$(basename "$file")
+
+        if [[ "$base_name" =~ $unwanted_characters_pattern ]] || [[ "$base_name" =~ $emoji_pattern ]]; then
+
+            echo "Found file that matches pattern(s): $file"
+        fi
+    done
 }
 
 if [ -z "$1" ]; then
