081b62b39d62b8fd0d147c4819e704abc32dd446
Francisco Martinez <franciscomj345@gmail.com>
2024-10-12 18:58:00 -0600

    handle unwanted_characters_pattern

--- rename_all_files.sh
+++ rename_all_files.sh
@@ -2,18 +2,26 @@
 
 
 unwanted_characters_pattern='[\/<>:"|?*\.]'
-unwanted_emoji_pattern='[\x{1F600}-\x{1F64F}\x{1F300}-\x{1F5FF}\x{1F680}-\x{1F6FF}\x{2600}-\x{26FF}\x{2700}-\x{27BF}]'
+#unwanted_emoji_pattern='[\x{1F600}-\x{1F64F}\x{1F300}-\x{1F5FF}\x{1F680}-\x{1F6FF}\x{2600}-\x{26FF}\x{2700}-\x{27BF}]'
 
-name_files_in_folder(){
+rename_files_in_folder(){
     local folder=$1
 
     find "$folder" -type f ! -name ".*" | while IFS= read -r file; do
+        dir_name=$(dirname "$file")
         base_name=$(basename "$file")
 
-        if [[ "$base_name" =~ $unwanted_characters_pattern ]] || [[ "$base_name" =~ $emoji_pattern ]]; then
+        echo -e "$base_name\n"
 
-            echo "Found file that matches pattern(s): $file"
+        new_base_name=$(echo "$base_name" | sed "s/$unwanted_characters_pattern//g")
+
+        if [ "$base_name" != "$new_base_name" ]; then
+            mv "$file" "$dir_name/$new_base_name"
+            echo -e "Renamed\n$new_base_name"
+        else
+            echo -e "Original kept\n$base_name\n"
         fi
+
     done
 }
 
@@ -22,4 +30,4 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-name_files_in_folder "$1"
+rename_files_in_folder "$1"
