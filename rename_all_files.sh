a6a8c5678604f33b73178b173d43489ddc53b138
Francisco Martinez <franciscomj345@gmail.com>
2024-10-12 18:12:09 -0600

    folder path

--- rename_all_files.sh
+++ rename_all_files.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 name_files_in_folder(){
+  local folder=$1
   echo "hello"
+  echo ${folder}
 }
 
-name_files_in_folder
+if [ -z "$1" ]; then
+    echo "missing folder path"
+    exit 1
+fi
+
+name_files_in_folder "$1"
