be0429370eaad8d0aebad928af65621c4052b455
Francisco Martinez <franciscomj345@gmail.com>
2024-10-12 18:29:46 -0600

    unwanted patterns

--- rename_all_files.sh
+++ rename_all_files.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+
+unwanted_characters_pattern='[\/<>:"|?*\.]'
+unwanted_emoji_pattern='[\x{1F600}-\x{1F64F}\x{1F300}-\x{1F5FF}\x{1F680}-\x{1F6FF}\x{2600}-\x{26FF}\x{2700}-\x{27BF}]'
+
 name_files_in_folder(){
-  local folder=$1
+    local folder=$1
   echo "hello"
   echo ${folder}
 }
